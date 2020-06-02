1adfa0f5c475ba978eccfe58b72c5b211f3efe44
VasylRyan <vasylrybalka@gmail.com>
2020-06-02 22:17:02 +0200

    edit file project1/application.py

--- project1/application.py
+++ project1/application.py
@@ -1,6 +1,6 @@
 import os
 
-from flask import Flask, session
+from flask import Flask, session, render_template, request
 from flask_session import Session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
@@ -21,6 +21,15 @@ engine = create_engine(os.getenv("DATABASE_URL"))
 db = scoped_session(sessionmaker(bind=engine))
 
 
+#@app.route("/")
+#def index():
+#    return "Project 1: TODO FROM VASYL"
+
 @app.route("/")
 def index():
-    return "Project 1: TODO FROM VASYL"
+    return render_template("index.html")
+
+@app.route("/hello", methods=["POST"])
+def hello():
+    name = request.form.get("name")
+    return render_template("hello.html", name=name)
