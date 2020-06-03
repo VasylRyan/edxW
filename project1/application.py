7e7149d31a6e8399f401af3e89336a3984e666d4
VasylRyan <vasylrybalka@gmail.com>
2020-06-03 18:22:57 +0200

    added books to table

--- project1/application.py
+++ project1/application.py
@@ -1,9 +1,15 @@
-import os
+import os, json
 
-from flask import Flask, session, render_template, request
+from flask import Flask, session, redirect, render_template, request, jsonify, flash
 from flask_session import Session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
+
+from werkzeug.security import check_password_hash, generate_password_hash
+
+import requests
+
+from helpers import login_required
 
 app = Flask(__name__)
 
@@ -21,15 +27,115 @@ engine = create_engine(os.getenv("DATABASE_URL"))
 db = scoped_session(sessionmaker(bind=engine))
 
 
-#@app.route("/")
-#def index():
-#    return "Project 1: TODO FROM VASYL"
-
 @app.route("/")
+@login_required
 def index():
+    """ Show search box """
+
     return render_template("index.html")
 
-@app.route("/hello", methods=["POST"])
-def hello():
-    name = request.form.get("name")
-    return render_template("hello.html", name=name)
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    """ Log user in """
+
+    # Forget any user_id
+    session.clear()
+
+    username = request.form.get("username")
+
+    # User reached route via POST (as by submitting a form via POST)
+    if request.method == "POST":
+
+        # Ensure username was submitted
+        if not request.form.get("username"):
+            return render_template("error.html", message="must provide username")
+
+        # Ensure password was submitted
+        elif not request.form.get("password"):
+            return render_template("error.html", message="must provide password")
+
+        # Query database for username (http://zetcode.com/db/sqlalchemy/rawsql/)
+        # https://docs.sqlalchemy.org/en/latest/core/connections.html#sqlalchemy.engine.ResultProxy
+        rows = db.execute("SELECT * FROM users WHERE username = :username",
+                            {"username": username})
+
+        result = rows.fetchone()
+
+        # Ensure username exists and password is correct
+        if result == None or not check_password_hash(result[2], request.form.get("password")):
+            return render_template("error.html", message="invalid username and/or password")
+
+        # Remember which user has logged in
+        session["user_id"] = result[0]
+        session["user_name"] = result[1]
+
+        # Redirect user to home page
+        return redirect("/")
+
+    # User reached route via GET (as by clicking a link or via redirect)
+    else:
+        return render_template("login.html")
+
+@app.route("/logout")
+def logout():
+    """ Log user out """
+
+    # Forget any user ID
+    session.clear()
+
+    # Redirect user to login form
+    return redirect("/")
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    """ Register user """
+
+    # Forget any user_id
+    session.clear()
+
+    # User reached route via POST (as by submitting a form via POST)
+    if request.method == "POST":
+
+        # Ensure username was submitted
+        if not request.form.get("username"):
+            return render_template("error.html", message="must provide username")
+
+        # Query database for username
+        userCheck = db.execute("SELECT * FROM users WHERE username = :username",
+                          {"username":request.form.get("username")}).fetchone()
+
+        # Check if username already exist
+        if userCheck:
+            return render_template("error.html", message="username already exist")
+
+        # Ensure password was submitted
+        elif not request.form.get("password"):
+            return render_template("error.html", message="must provide password")
+
+        # Ensure confirmation wass submitted
+        elif not request.form.get("confirmation"):
+            return render_template("error.html", message="must confirm password")
+
+        # Check passwords are equal
+        elif not request.form.get("password") == request.form.get("confirmation"):
+            return render_template("error.html", message="passwords didn't match")
+
+        # Hash user's password to store in DB
+        hashedPassword = generate_password_hash(request.form.get("password"), method='pbkdf2:sha256', salt_length=8)
+
+        # Insert register into DB
+        db.execute("INSERT INTO users (username, hash) VALUES (:username, :password)",
+                            {"username":request.form.get("username"),
+                             "password":hashedPassword})
+
+        # Commit changes to database
+        db.commit()
+
+        flash('Account created', 'info')
+
+        # Redirect user to login page
+        return redirect("/login")
+
+    # User reached route via GET (as by clicking a link or via redirect)
+    else:
+        return render_template("register.html")
